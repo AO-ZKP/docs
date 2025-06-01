c37fbad5c3208ffb004b96b53c951042a80bb882
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2025-06-02 01:17:29 +0530

    port to basic vocs

---
+++ vocs.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "A0 Labs Docs",
+  sidebar: [
+    {
+      text: "AO",
+      link: "/ao",
+    },
+    {
+      text: "AOS",
+      link: "/aos",
+    },
+    {
+      text: "Lua Rust Starter",
+      link: "/lua-rust-starter",
+    },
+  ],
+});
